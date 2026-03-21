97156430d9a0832c04c6f5b1ae1f6a13e2b15aff
Pys0n <jason.krueger2010@web.de>
2026-03-21 13:32:26 +0100

    wordle added

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Simple Minigames developed in Python.
 
 The Minigames are based on classes, so you can extend them, use them to learn programming or build your own bots and algorithms to play against you, your friends and their bots.
 
-There are currently two minigames:
-- TicTacToe (`tictactoe.py`)
+There are currently three minigames:
 - Four in A Row (`fourinarow.py`)
+- TicTacToe (`tictactoe.py`)
+- Wordle (`wordle.py`)
+    - **Wordle Word List:** https://gist.github.com/dracos/dd0668f281e685bad51479e5acaadb93
 
 There is currently one bot:
 - TicTacToe-Bot (`tictactoe_bot.py`)
---
+++ wordle.py
@@ -0,0 +1,70 @@
+import random, collections, os
+
+CLEAR = 'cls' if os.name == 'nt' else 'clear'
+
+class Wordle:
+    def __init__(self, tries: int = 6) -> None:
+        self.all_words = self.load_words()
+        self.word = random.choice(list(self.all_words))
+        self.tries = tries
+
+    
+    def load_words(self) -> set:
+        words = set()
+
+        with open('valid-wordle-words.txt', 'r', encoding='utf-8') as file:
+            for line in file.readlines():
+                words.add(line.strip().upper())
+        
+        return words
+
+    
+    def guess(self, word: str) -> tuple[bool, str]:
+        word = word.upper()
+
+        letters = set(list(self.word))
+        result = ''
+
+        if word != self.word:
+            for pos, letter in enumerate(word):
+                if letter not in self.word:
+                    result += letter
+                elif self.word[pos] == letter:
+                    result += f'\033[42m{letter}\033[0m'
+                else:
+                    result += f'\033[43m{letter}\033[0m'
+            
+            self.tries -= 1
+            
+            return (False, result)
+        else:
+            return (True, f'\033[42m{word}\033[0m')
+
+
+guessed = False
+guesses = []
+wordle = Wordle()
+while wordle.tries > 0:
+    os.system(CLEAR)
+    for guess in guesses:
+        print(guess)
+    
+    guess = input('\nYour Guess: ').upper()
+
+    if guess in wordle.all_words:
+        result = wordle.guess(guess)
+
+        guesses.append(result[1])
+
+        if result[0]:
+            guessed = True
+            break
+    else:
+        input('Word not in list! Press <Enter> to continue...')
+
+os.system(CLEAR)
+
+for guess in guesses:
+    print(guess)
+
+if not guessed: print('\nSolution: ' + wordle.word)
